ea7ac88f0c3c204411242f07c65bafe3478f6512
estin7874 <estin7874@gmail.com>
2014-05-18 14:07:36 +0900

    Fix shell output.

--- misc/es/delete_indices.sh
+++ misc/es/delete_indices.sh
@@ -2,10 +2,10 @@
 
 . `dirname $0`/../settings.sh
 
-echo '[stop river tweets]'
+echo '[delete index tweets]'
 curl -XDELETE http://$ES_HOST:$ES_PORT/tweets
 echo ''
 
-echo '[stop river users]'
+echo '[delete index users]'
 curl -XDELETE http://$ES_HOST:$ES_PORT/users
 echo ''
